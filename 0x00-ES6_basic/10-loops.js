8a794fa8cd8145af6c11baca852fec09eed6be9e
Magnus984 <nharnharyhaw984@gmail>
2024-09-30 23:31:06 +0000

    Add: 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const word of array) {
+    newArray.push(appendString + word);
+  }
+
+  return newArray;
+}

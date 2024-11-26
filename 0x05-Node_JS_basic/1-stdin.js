c7c0c0a641d8ffdbb2202c8b0adaed3d14cbcd22
Magnus <nharnharyhaw984@gmail.com>
2024-11-26 22:25:59 +0000

    Mod

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
-  console.log('This important software is now closing ');
 });
+process.stdin.on('end', function() {
+    process.stdout.write('This important software is now closing\n');
+  });
